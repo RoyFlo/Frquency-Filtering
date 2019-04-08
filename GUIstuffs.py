2b57b2a544952d706cf24b15d35da8e2f2624cf7
RoyFlo <43392885+RoyFlo@users.noreply.github.com>
2019-04-08 13:10:01 -0500

    Update GUIstuffs.py

--- GUIstuffs.py
+++ GUIstuffs.py
@@ -16,7 +16,7 @@ Label(window, text="1. Select an Image", font=("Ariel", 12), fg="blue").grid(row
 Label(window, text="2. Select Filter", font=("Ariel", 12), fg="blue").grid(row=0, column=1)
 Label(window, text="3. Enter Cutoff", font=("Ariel", 12), fg="blue").grid(row=0, column=2)
 Label(window, text="4. Enter Order", font=("Ariel", 12), fg="blue").grid(row=0, column=3)
-# Label(window, text="5. RUN", font=("Ariel", 12), fg="blue").grid(row=0, column=5)
+# Label(window, text="5. RUN", font=("Ariel", 12), fg="blue").grid(row=0, column=4)
 
 
 
@@ -48,6 +48,18 @@ def run():
     print("Cutoff = ", cutoff)
     print("Order = ", order)
 
+    image = imread(img, as_gray=True)
+
+    a1 = fig.add_subplot(221)
+    a1.imshow(image, cmap=plt.cm.Greys_r)
+    a1.set_title("Original Image")
+
+    fig.tight_layout()
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.get_tk_widget().grid(row=2, columnspan=5)
+    Label(window, text=rerror, font=("Ariel", 10), fg="red").grid(row=3, sticky=SE)
+    canvas.draw()
+
 
 iList = ["Image1", "Image2", "Image3", "Image4", "Image5", "Image6"]
 filterList = ["Ideal High Pass", "Ideal Low Pass", "Gaussian High Pass", "Gaussian Low Pass", "Butterworth High Pass",
@@ -56,38 +68,37 @@ filterList = ["Ideal High Pass", "Ideal Low Pass", "Gaussian High Pass", "Gaussi
 # Image
 var1 = StringVar()
 var1.set("Image1")
-
 # Filter
 var2 = StringVar()
 var2.set("Ideal High Pass")
-
 # Cutoff
 var3 = StringVar()
 var3.set(15)
-
 # Order
 var4 = StringVar()
 var4.set(2)
 
+# Image menu
 set1 = OptionMenu(window, var1, *iList, command=iValue)
 set1.configure(font=("Ariel"))
 set1.grid(row=1, column=0)
-
+# Filter menu
 set2 = OptionMenu(window, var2, *filterList, command=fValue)
 set2.configure(font=("Ariel"))
 set2.grid(row=1, column=1)
-
+# Cutoff Entry
 setCutoff = Entry(window, textvariable=var3)
 setCutoff.grid(row=1, column=2)
-
+# Order Entry
 setOrder = Entry(window, textvariable=var4)
 setOrder.grid(row=1, column=3)
 
 
 fig = Figure(figsize=(7, 7))
+canvas = FigureCanvasTkAgg(fig, master=window)
 
 # RUN button
 button1 = Button(window, text="**RUN**", bg="red", font=("Ariel", 12), command=run)
-button1.grid(row=1, column=5, padx=30, pady=15)
+button1.grid(row=1, column=4, padx=30, pady=15)
 
 window.mainloop()
