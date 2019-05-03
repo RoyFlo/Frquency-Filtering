d014e8044222c4a3b78d239794560c9364f8770f
Edrienne <agustin.isse@gmail.com>
2019-05-03 10:40:41 -0500

    avoid gui uncaught error on mac

--- GUIstuffs.py
+++ GUIstuffs.py
@@ -1,3 +1,9 @@
+# to run on MAC OS (avoid uncaught error for matplotlib)
+from sys import platform as sys_pf
+if sys_pf == 'darwin':
+    import matplotlib
+    matplotlib.use("TkAgg")
+
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 import matplotlib.pyplot as plt
